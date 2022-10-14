b2c88a30b1ddf5c69e6be25905a21af95adc45f8
suparansharma <suparansharmap@gmail.com>
2022-10-15 01:44:56 +0600

    expert item done

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -14,7 +14,7 @@ const Services = () => {
 
     return (
         <div>
-            <h2 className='services-title' >Our Services</h2>
+            <h2 className='services-title mt-5' >Our Services</h2>
             <div className="services-container">
             {
                 services.map(service=> <Service
