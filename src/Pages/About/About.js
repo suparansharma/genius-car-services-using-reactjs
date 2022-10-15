cb10cd1e6b92e52040441e85ecd5df74839e39b0
suparansharma <suparansharmap@gmail.com>
2022-10-15 13:05:10 +0600

    navbar done with dynamic link

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            
+            <h2>this is all abput us</h2>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,22 +1,43 @@
 import React from 'react';
+import { NavDropdown } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 import logo from '../../../images/logo.png'
 
 
 const Header = () => {
     return (
-        <Navbar bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home"><img src={logo} height="30px" alt="" /></Navbar.Brand>
+      <Navbar sticky='top' collapseOnSelect expand="lg" bg="primary" variant="dark">
+      <Container>
+        <Navbar.Brand as={Link} to="/"> <img height={30} src={logo}/> </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
-        </Container>
-      </Navbar>
+          <Nav>
+            <Nav.Link as={Link} to="about">About</Nav.Link>
+            <Nav.Link as={Link} to="login">
+              Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 
     );
 };
