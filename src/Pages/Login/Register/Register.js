c62cb76988522a97215b908bbdfdbcc4d0254193
suparansharma <suparansharmap@gmail.com>
2022-10-17 00:23:32 +0600

    Implement Google login system

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import './Register.css'
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Register = () => {
     
@@ -41,6 +42,7 @@ const Register = () => {
                 <input type="submit" value="Register"/>
             </form>
             <p>Already have an account? <Link to="/login" className="text-danger pe-auto text-decoration-none" onClick={navigateLogin} >Please Login</Link> </p>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
