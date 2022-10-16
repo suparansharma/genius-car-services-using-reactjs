76a990e81bc37c3aee3495c4da1f03bb2a66bd59
suparansharma <suparansharmap@gmail.com>
2022-10-16 10:54:12 +0600

    register form done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from './Pages/Shared/Footer/Footer';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import NotFound from './Pages/Shared/NotFound/NotFound';
 import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail> }></Route>
       <Route path='/about' element={<About></About> }></Route>
       <Route path='/login' element={<Login></Login>}></Route>
+      <Route path='/register' element={<Register></Register>}></Route>
       <Route path="*" element={<NotFound></NotFound>}></Route>
      </Routes>
      <Footer></Footer>
