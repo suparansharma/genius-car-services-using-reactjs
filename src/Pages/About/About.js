5677d93e78d0922a04edc87c916a9248c04392c1
suparansharma <suparansharmap@gmail.com>
2022-10-17 22:16:49 +0600

    dynamic title

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import PageTitle from '../Shared/PageTitle/PageTitle';
 
 const About = () => {
     return (
         <div>
+            <PageTitle title="About"></PageTitle>
             <h2>this is all abput us</h2>
         </div>
     );
